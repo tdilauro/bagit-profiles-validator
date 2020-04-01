a877f9600fe4d6e1d9307f2c2631a01c1a3c419e
Tim DiLauro <tim.di.lauro@gmail.com>
2020-03-31 23:26:13 -0400

    new profile (not bag) validator and associated tests

---
+++ profile_validator.py
@@ -0,0 +1,124 @@
+from collections import namedtuple
+import logging
+import sys
+if sys.version_info > (3,):
+    basestring = str
+else:
+    basestring = basestring
+
+_Test = namedtuple('_Test', ['condition', 'severity', 'message'])
+_ERROR = "E"
+_WARNING = "W"
+
+
+class ProfileValidator(object):
+
+    def __init__(self, profile, report=None, enable_logging=False, fail_on_warning=False):
+        self.profile = profile
+        self.report = report
+        self.logging_enabled = enable_logging
+        self.fail_on_warning = fail_on_warning
+
+    def _error(self, msg):
+        if self.report is not None:
+            self.report.error(msg)
+        if self.logging_enabled:
+            logging.error(msg)
+        return False
+
+    def _warning(self, msg):
+        if self.report is not None:
+            self.report.warning(msg)
+        if self.logging_enabled:
+            logging.warning(msg)
+        return False if self.fail_on_warning else True
+
+    def validate(self):
+        valid = True
+        valid &= self._validate_bagit_profile_info()
+        valid &= self._validate_accept_bagit_versions()
+        valid &= self._validate_bag_info()
+        valid &= self._validate_manifests_allowed()
+        return valid
+
+    # Check self.profile['bag-profile-info'] to see if "Source-Organization",
+    # "External-Description", "Version" and "BagIt-Profile-Identifier" are present.
+    def _validate_bagit_profile_info(self):
+        valid = True
+        if "BagIt-Profile-Info" not in self.profile:
+            valid &= self._error("%s: Required 'BagIt-Profile-Info' section is missing." % self.profile)
+        else:
+            valid &= self._validate_bagit_profile_info_children()
+        return valid
+
+    def _validate_bagit_profile_info_children(self):
+        valid = True
+        if "Source-Organization" not in self.profile["BagIt-Profile-Info"]:
+            valid &= self._error(
+                "%s: Required 'Source-Organization' property is not in 'BagIt-Profile-Info'."
+                % self.profile
+            )
+        if "Version" not in self.profile["BagIt-Profile-Info"]:
+            valid &= self._warning(
+                "%s: Required 'Version' property is not in 'BagIt-Profile-Info'." % self.profile
+            )
+        if "BagIt-Profile-Identifier" not in self.profile["BagIt-Profile-Info"]:
+            valid &= self._error(
+                "%s: Required 'BagIt-Profile-Identifier' property is not in 'BagIt-Profile-Info'."
+                % self.profile
+            )
+
+        return valid
+
+    def _validate_accept_bagit_versions(self):
+        """
+        Ensure all versions in 'Accept-BagIt-Version' are strings
+        """
+        valid = True
+        if "Accept-BagIt-Version" in self.profile:
+            for version_number in self.profile["Accept-BagIt-Version"]:
+                if not isinstance(version_number, basestring):
+                    valid &= self._error(
+                        'Version number "%s" in "Accept-BagIt-Version" is not a string!' % version_number
+                    )
+        return valid
+
+    def _validate_bag_info(self):
+        valid = True
+        profile = self.profile
+        if 'Bag-Info' in profile:
+            for tag in profile['Bag-Info']:
+                config = profile['Bag-Info'][tag]
+                if profile.profile_version_info >= (1, 3, 0) and \
+                        'description' in config and not isinstance(config['description'], basestring):
+                    valid &= self._warning(
+                        "%s: Profile Bag-Info '%s' tag 'description' property, when present, must be a string." %
+                        (profile, tag)
+                    )
+        return valid
+
+    def _validate_manifests_allowed(self):
+        valid = True
+        valid &= self._validate_allowed_manifest_type(manifest_type="tag",
+                                                      allowed_attribute="Tag-Manifests-Allowed",
+                                                      required_attribute="Tag-Manifests-Required")
+        valid &= self._validate_allowed_manifest_type(manifest_type="payload",
+                                                      allowed_attribute="Manifests-Allowed",
+                                                      required_attribute="Manifests-Required")
+        return valid
+
+    def _validate_allowed_manifest_type(self, manifest_type=None, allowed_attribute=None, required_attribute=None):
+        valid = True
+        if allowed_attribute not in self.profile:
+            return valid
+
+        allowed = self.profile[allowed_attribute]
+        required = self.profile[required_attribute] if required_attribute in self.profile else []
+
+        required_but_not_allowed = [alg for alg in required if alg not in allowed]
+        if required_but_not_allowed:
+            valid &= self._error(
+                "%r: Required %s manifest type(s) %s not allowed by %s" %
+                (self.profile, manifest_type, [str(a) for a in required_but_not_allowed], allowed_attribute)
+            )
+        return valid
---
+++ test_profile_validator.py
@@ -0,0 +1,169 @@
+from copy import deepcopy
+import json
+import unittest
+
+from profile import Profile
+from report import Report
+from profile_validator import ProfileValidator
+
+
+class TestProfileValidator(unittest.TestCase):
+
+    def setUp(self):
+        json_string = sample_profile_v1_3_0()
+        self.profile_dict = json.loads(json_string)
+        self.profile = Profile.from_json(json_string)
+
+    def _get_safe_profile_dict(self):
+        return deepcopy(self.profile_dict)
+
+    def test_ValidationOfValidProfileShouldSucceed(self):
+        report = Report()
+        self.validator = ProfileValidator(self.profile, report=report, fail_on_warning=False)
+        success = self.validator.validate()
+        self.assertTrue(success)
+
+    def test_BagItProfileInfoSectionMustBePresent(self):
+        report = Report()
+        profile_dict = self._get_safe_profile_dict()
+        del(profile_dict["BagIt-Profile-Info"])
+        profile = Profile.from_dict(profile_dict)
+        validator = ProfileValidator(profile, report=report)
+        success = validator.validate()
+        self.assertFalse(success)
+        self.assertGreater(len(profile), 0)
+        self.assertIn("Required 'BagIt-Profile-Info' section is missing", report.error_messages()[0])
+
+    def test_BagInfoTagDescriptionShouldNotBeABooleanGivesWarning(self):
+        report = Report()
+        profile_dict = self._get_safe_profile_dict()
+        profile_dict["Bag-Info"] = {"FakeTag": {"description": False}}
+        profile = Profile.from_dict(profile_dict)
+        validator = ProfileValidator(profile, report=report)
+        validator.validate()
+        self.assertEqual(len(report.warning_messages()), 1)
+        self.assertIn("tag 'description' property, when present, must be a string.", report.warning_messages()[0])
+
+    def test_BagInfoTagDescriptionShouldNotBeANumberGivesWarning(self):
+        report = Report()
+        profile_dict = self._get_safe_profile_dict()
+        profile_dict["Bag-Info"] = {"FakeTag": {"description": 0}}
+        profile = Profile.from_dict(profile_dict)
+        validator = ProfileValidator(profile, report=report)
+        validator.validate()
+        self.assertEqual(len(report.warning_messages()), 1)
+        self.assertIn("tag 'description' property, when present, must be a string.", report.warning_messages()[0])
+
+    def test_BagInfoTagDescriptionShouldBeAString(self):
+        report = Report()
+        profile_dict = self._get_safe_profile_dict()
+        profile_dict["Bag-Info"] = {"FakeTag": {"description": "some string"}}
+        profile = Profile.from_dict(profile_dict)
+        validator = ProfileValidator(profile, report=report)
+        success = validator.validate()
+        self.assertTrue(success)
+        self.assertEqual(len(report.messages()), 0)
+
+    def test_WarningShouldResultInFailureWhenFailOnWarningTrue(self):
+        report = Report()
+        profile_dict = self._get_safe_profile_dict()
+        profile_dict["Bag-Info"] = {"FakeTag": {"description": False}}
+        profile = Profile.from_dict(profile_dict)
+        validator = ProfileValidator(profile, report=report, fail_on_warning=True)
+        success = validator.validate()
+        self.assertFalse(success)
+        self.assertEqual(len(report.warning_messages()), 1)
+
+    def test_WarningShouldNotCauseFailureWhenFailOnWarningFalse(self):
+        report = Report()
+        profile_dict = self._get_safe_profile_dict()
+        profile_dict["Bag-Info"] = {"FakeTag": {"description": False}}
+        profile = Profile.from_dict(profile_dict)
+        validator = ProfileValidator(profile, report=report, fail_on_warning=False)
+        success = validator.validate()
+        self.assertTrue(success)
+        self.assertEqual(len(report.warning_messages()), 1)
+
+    def test_WarningShouldNotCauseFailureWhenFailOnWarningUnset(self):
+        report = Report()
+        profile_dict = self._get_safe_profile_dict()
+        profile_dict["Bag-Info"] = {"FakeTag": {"description": False}}
+        profile = Profile.from_dict(profile_dict)
+        validator = ProfileValidator(profile, report=report)
+        success = validator.validate()
+        self.assertTrue(success)
+        self.assertEqual(len(report.warning_messages()), 1)
+
+    def test_PayloadManifestAllowedRequiredConsistency(self):
+        report = Report()
+        profile_dict = self._get_safe_profile_dict()
+        profile_dict["Manifests-Allowed"] = ["foo"]
+        profile_dict["Manifests-Required"] = ["sha256"]
+        profile = Profile.from_dict(profile_dict)
+        validator = ProfileValidator(profile, report=report)
+        success = validator.validate()
+        self.assertFalse(success)
+        self.assertEqual(len(report.error_messages()), 1)
+        self.assertRegexpMatches(report.error_messages()[0],
+                                 r'Required payload manifest type.+not allowed by Manifests-Allowed')
+
+    def test_TagManifestAllowedRequiredConsistency(self):
+        report = Report()
+        profile_dict = self._get_safe_profile_dict()
+        profile_dict["Tag-Manifests-Allowed"] = ["foo"]
+        profile_dict["Tag-Manifests-Required"] = ["sha256"]
+        profile = Profile.from_dict(profile_dict)
+        validator = ProfileValidator(profile, report=report)
+        success = validator.validate()
+        self.assertFalse(success)
+        self.assertEqual(len(report.error_messages()), 1)
+        self.assertRegexpMatches(report.error_messages()[0],
+                                 r'Required tag manifest type.+not allowed by Tag-Manifests-Allowed')
+
+    def test_AllowMorePayloadManifestsThanRequired(self):
+        report = Report()
+        profile_dict = self._get_safe_profile_dict()
+        profile_dict["Manifests-Allowed"] = ["sha256", "sha512"]
+        profile_dict["Manifests-Required"] = ["sha256"]
+        profile = Profile.from_dict(profile_dict)
+        validator = ProfileValidator(profile, report=report)
+        success = validator.validate()
+        self.assertTrue(success)
+        self.assertEqual(len(report.messages()), 0)
+
+    def test_AllowMoreTagManifestsThanRequired(self):
+        report = Report()
+        profile_dict = self._get_safe_profile_dict()
+        profile_dict["Tag-Manifests-Allowed"] = ["sha256", "sha512"]
+        profile_dict["Tag-Manifests-Required"] = ["sha256"]
+        profile = Profile.from_dict(profile_dict)
+        validator = ProfileValidator(profile, report=report)
+        success = validator.validate()
+        self.assertTrue(success)
+        self.assertEqual(len(report.messages()), 0)
+
+
+def sample_profile_v1_3_0():
+    return """
+        {
+           "BagIt-Profile-Info": {
+              "BagIt-Profile-Identifier": "TEST",
+              "BagIt-Profile-Version": "1.3.0",
+              "Source-Organization": "bagit-profiles.py",
+              "Contact-Name": "John Doe",
+              "Contact-Email": "johndoe@example.org",
+              "External-Description": "Test Profile",
+              "Version": "1.0"
+           },
+           "Bag-Info": {},
+           "Manifests-Required": ["sha256", "sha512"],
+           "Tag-Manifests-Required": ["sha256", "sha512"],
+           "Tag-Files-Required": [],
+           "Allow-Fetch.txt": true,
+           "Accept-BagIt-Version": ["0.97"]
+        }
+        """
+
+
+if __name__ == '__main__':
+    unittest.main()
